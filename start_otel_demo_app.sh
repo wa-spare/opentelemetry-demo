1618609db99c2773058d39fc84b366f9024810f7
brave <wa_spare@hotmail.com>
2023-08-25 16:24:36 -0400

    Update start_otel_demo_app.sh

--- start_otel_demo_app.sh
+++ start_otel_demo_app.sh
@@ -1,3 +1,3 @@
-export DT_API_TOKEN=dt0c01.{DT_API_TOKEN}
-export DT_OTLP_ENDPOINT=https://{DT_TENANT}.live.dynatrace.com/api/v2/otlp
+export DT_API_TOKEN=dt0c01.{your DT API Token}
+export DT_OTLP_ENDPOINT=https://{your DT Tenant Id}.live.dynatrace.com/api/v2/otlp
 docker compose up --no-build
